f7bdc0f290dd1e2e6942422443bbd301f414f0c1
Asher <95162513+AGrey48@users.noreply.github.com>
2024-08-18 00:27:13 -0600

    Created timer logic for pomodoro.
    Next goal: Add the logic for the pomodoro session

--- Pomodoro.cs
+++ Pomodoro.cs
@@ -12,7 +12,13 @@ namespace LittleFunProjects
 {
     public partial class Pomodoro : Form
     {
-        int ticker;
+        private int defaultSeconds = 3;
+        private int defaultMinutes = 25;
+
+        private int currentMinutes;
+        private int currentSeconds;
+
+        private int iteration = 0;
         public Pomodoro()
         {
             InitializeComponent();
@@ -20,12 +26,104 @@ namespace LittleFunProjects
 
         private void returnToMainMenuToolStripMenuItem_Click(object sender, EventArgs e) { this.Close(); }
 
+        // fires this function with every tick
         private void countdown_Tick(object sender, EventArgs e)
         {
-            string mystr = ticker.ToString();
+            // count seconds all the way down to zero
+            if (defaultSeconds > 0)
+            {
+                defaultSeconds--;
+                SecondDisplay.Text = defaultSeconds + " seconds";
+            }
 
-            ticker = ticker + 1;
+            // decrement minutes, reset seconds to 59
+            else if (defaultSeconds == 0)
+            {
+                defaultMinutes--;
+                MinuteDisplay.Text = defaultMinutes + " minutes";
+                defaultSeconds = 59;
+                SecondDisplay.Text = defaultSeconds + " seconds";
+            }
 
+        }
+
+        // when countdown hits 0:00, let user know
+        private void EndSession()
+        {
+
+        }
+
+        // default settings for short break (forgot to change the name)
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) 
+        { 
+            MinuteDisplay.Text = "7 Minutes"; 
+            SecondDisplay.Text = "30 Seconds";
+
+            defaultMinutes = 7;
+            defaultSeconds = 30;
+        }
+
+        // default settings for long break (forgot to change the name)
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) 
+        { 
+            MinuteDisplay.Text = "12 Minutes"; 
+            SecondDisplay.Text = "30 Seconds";
+
+            defaultMinutes = 12;
+            defaultSeconds = 30;
+        }
+
+        // default settings for pomodoro timer
+        private void PomoBtn_CheckedChanged(object sender, EventArgs e) 
+        { 
+            MinuteDisplay.Text = "15 Minutes"; 
+            SecondDisplay.Text = "30 Seconds";
+
+            defaultMinutes = 15;
+            defaultSeconds = 30;
+        }
+
+        private void startBtn_Click(object sender, EventArgs e) { countdown.Start(); }
+
+        private void stopBtn_Click(object sender, EventArgs e) { countdown.Stop(); }
+
+        // check which radio button is selected and reset time depending on which button is selected
+        private void resetBtn_Click(object sender, EventArgs e) 
+        {
+            countdown.Stop();
+            
+            if (PomoBtn.Checked)
+            {
+                MinuteDisplay.Text = "15 Minutes"; SecondDisplay.Text = "30 Seconds";
+            }
+
+            else if (ShortBreakBtn.Checked) 
+            {
+                MinuteDisplay.Text = "7 Minutes";
+                SecondDisplay.Text = "30 Seconds";
+
+                defaultMinutes = 7;
+                defaultSeconds = 30;
+
+            }
+
+            else if (LongBreakBtn.Checked) 
+            {
+                MinuteDisplay.Text = "12 Minutes";
+                SecondDisplay.Text = "30 Seconds";
+
+                defaultMinutes = 12;
+                defaultSeconds = 30;
+            }
+
+            else if (PomoBtn.Checked)
+            {
+                MinuteDisplay.Text = "15 Minutes";
+                SecondDisplay.Text = "30 Seconds";
+
+                defaultMinutes = 15;
+                defaultSeconds = 30;
+            }
         }
     }
 }
